649733796b9054fb79430c89113f0644e32a15cd
Sajeevan98 <sajee91098@gmail.com>
2025-07-08 13:49:22 +0530

    #1.2, configure Tailwind CSS integration with React project

--- designer/client/src/App.jsx
+++ designer/client/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello! This is Sajeevan.</h1>
+      <h1 className="text-yellow-500 underline font-extrabold text-5xl">Hello! This is Sajeevan.</h1>
     </>
   )
 }
